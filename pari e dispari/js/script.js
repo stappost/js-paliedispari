a8c430d3de8ff4252c15f5562dfcd6804fa62a5b
stappost <emanuele.scarpantoni@gmail.com>
2023-11-28 17:26:53 +0100

    aggiunta funzione pari o dispari e somma

--- pari e dispari/js/script.js
+++ pari e dispari/js/script.js
@@ -3,6 +3,7 @@ function random(){
     let num = Math.floor(Math.random() * 5 + 1);
     return num;
 }
+
 // 2 - permettiamo all'utente di inserire un numero casuale da 1 a 5;
 let correct = false;
 let number;
@@ -15,12 +16,33 @@ while(!correct){
 
 
 // 3 - permettiamo all'utente di scegliere tra pari e dispari;
-let choise = prompt("pari o dispari?");
+correct = false;
+let choise;
+while(!correct){
+    choise = prompt("pari o dispari?");
+    if(choise == "pari" || choise == "dispari"){
+        correct = true;
+    }
+}
 // 4 - sommiamo i due numeri
+let pc_num = random()
+console.log(pc_num)
+let somma = pc_num + number
+
 // 5 - in una funzione "pariDispari" stabiliamo se i due numeri sono pari o dispari
 //     6 - ? sono pari :
 //         7.1 -  restituiamo "pari"
-//     8 - : restituiamo "dispari"
+function oddOrEven(){
+    if(somma%2 == 0){
+        return "pari"
+    }
+    //     8 - : restituiamo "dispari"
+    else{
+        return "dispari"
+    }
+}
+let result = oddOrEven()
+console.log(result)
 // 9 - ? è uscito pari e l'utente ha scelto pari O è uscito dispari e l'utente ha scelto dispari
 //     9.1 - stampiamo il risultato della  funzione  "pariDispari" più "hai vinto";
 // 10 - :  stampiamo il risultato della  funzione  "pariDispari" più "ha vinto il computer";
