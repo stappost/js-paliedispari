de242d14f46fc19115c64403c9b9ac5ffc9babb4
stappost <emanuele.scarpantoni@gmail.com>
2023-11-28 16:42:06 +0100

    finito es PALINDROMA

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -5,7 +5,6 @@ function palindroma(parola){
     //     3 - trasformare parola in array,capovolgiamola e ritrasformiamola in stringa facendo attenzione che sia tutto in minuscolo;
     let parole
     parole = parola.split("").reverse().join("").toLowerCase();
-    console.log(parole)
     
     return parole;
 }
@@ -14,4 +13,10 @@ console.log(parola)
 //     4 - confrontiamo il risultato con la parola inserita;
 //     5 - ? sono uguali
 //         5.1 - stampiamo "la parola è palindroma";
+if(word.toLowerCase() == parola){
+    console.log(`La parola ${word} è palindroma`)
+}
 //     6 - :  stampiamo "la parola NON è palindroma";
+else{
+    console.log(`La parola ${word} NON è palindroma`)
+}
