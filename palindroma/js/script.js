efc340a424a3d2feeda05605d8af9f8f60ed66fe
stappost <emanuele.scarpantoni@gmail.com>
2023-11-28 16:38:25 +0100

    aggiunto prompt e funzione in palindroma

---
+++ palindroma/js/script.js
@@ -0,0 +1,17 @@
+// 1 - permettere all'utente di inserire una parola;
+let word = prompt("aggiungere parola che si vuole controllare");
+// 2 - creare una funzione con parametro "parola";
+function palindroma(parola){
+    //     3 - trasformare parola in array,capovolgiamola e ritrasformiamola in stringa facendo attenzione che sia tutto in minuscolo;
+    let parole
+    parole = parola.split("").reverse().join("").toLowerCase();
+    console.log(parole)
+    
+    return parole;
+}
+let parola = palindroma(word)
+console.log(parola)
+//     4 - confrontiamo il risultato con la parola inserita;
+//     5 - ? sono uguali
+//         5.1 - stampiamo "la parola è palindroma";
+//     6 - :  stampiamo "la parola NON è palindroma";
